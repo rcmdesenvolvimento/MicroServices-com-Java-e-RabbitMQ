403f7812f301e86564036beb2332cac0c38e4b29
= <=>
2024-07-27 19:53:06 -0300

    Commit MicroServices notificacao-app

---
+++ MicroServices com Java e RabbitMQ/notificao-app/src/main/java/com/rcminfo/notificacao/app/NotificaoAppApplication.java
@@ -0,0 +1,13 @@
+package com.rcminfo.notificacao.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificaoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificaoAppApplication.class, args);
+	}
+
+}
